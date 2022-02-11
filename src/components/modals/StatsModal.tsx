a19025a807342fbb324a3ba499b36ea409cce2ab
JoenPoenMan <jeroenkrijgveel@gmail.com>
2022-02-11 20:48:52 +0100

    prince
    ali b

--- src/components/modals/StatsModal.tsx
+++ src/components/modals/StatsModal.tsx
@@ -11,7 +11,7 @@ import {
   NEW_WORD_TEXT,
   SHARE_TEXT,
 } from '../../constants/strings'
-import getUsed from './../../App'
+import hintUsed from './../../App'
 
 type Props = {
   isOpen: boolean
@@ -70,7 +70,7 @@ export const StatsModal = ({
             type="button"
             className="mt-2 w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-600 text-base font-medium text-white hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 sm:text-sm"
             onClick={() => {
-              shareStatus(guesses, isGameLost, getUsed())
+              shareStatus(guesses, isGameLost, hintUsed)
               handleShare()
             }}
           >
