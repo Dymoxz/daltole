9296b8165791063ea1406ab2090599e6642dac2b
Dymo Waltheer <210532@edu.ozhw.nl>
2022-02-15 11:35:23 +0100

    EZ

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -43,7 +43,7 @@ export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
   const epochMs = new Date('Februari 10, 2022 00:00:00').valueOf()
   const now = Date.now()
-  const msInDay = 180000
+  const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
   const nextday = (index + 1) * msInDay + epochMs
   const hintDay = false
